14be2bc7ca272669721e282dfd9f134984ac6ae0
Efremov Denis <ruzs.efremov@mail.ru>
2020-05-20 16:56:46 +0300

    Revert "mywork3"
    This reverts commit 7f4667637558e3535e68a26a7f3e60b728137a57.

--- Personal_data.py
+++
@@ -1,5 +0,0 @@
-def PersonalData (name, sirname, city):
-	result = name + sirname + city
-	return result
-
-print('first worker' + PersonalData(sirname="B", name="A", city="C"))
--- Sum.py
+++
@@ -1,9 +0,0 @@
-def MyFunc (a, b, c):
-	if a < b and a < c:
-		return b + c
-	elif b < a and b < c:
-		return a + c
-	else:
-		return a + b
-number = 2
-print(number + MyFunc (1, 2, 3))
